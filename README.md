1fe7a0b88b166a795f355620c94b0a63b4b0e192
Erin Grasmick <ecg@unseen.is>
2014-08-31 19:43:44 -0500

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ emailsnap
 =========
 
 A script to take a snap from a Thinkpad webcam using the ThinkVantage button and email it somewhere.
+
+Dependencies require VLC, Thunderbird/Icedove, xbindkeys and ImageMagick (though you may not need it).
+
+You will have to edit both files to fill in your username/path to the script and any individual image tweaks your webcam snap may need via Imagemagick.
---
+++ email.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+vlc -I dummy v4l2:///dev/video0 --video-filter scene --no-audio --scene-path /home/centauri/images/webcam --scene-prefix image_prefix --scene-format png vlc://quit --run-time=1
+NAME='/home/USER/images/webcam/image_prefix00051.png'
+convert $NAME -flip current.jpg
+thunderbird -compose "to=email@email.edu,subject=subject,attachment=file:///home/USER/images/webcam/current.jpg" &
+sleep 1
+xdotool key Control_L+Return
