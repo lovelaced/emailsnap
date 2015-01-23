7c6d7e0cb29aaa1230e4b14b7bdc0333205f9f5f
lovelaced <ecg@unseen.is>
2015-01-23 16:26:56 +0000

    Update email.sh

--- email.sh
+++ email.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-vlc -I dummy v4l2:///dev/video0 --video-filter scene --no-audio --scene-path '/home/'$USER'/images/webcam' --scene-prefix image_prefix --scene-format png vlc://quit --run-time=1
-NAME = $(echo $HOME'/images/webcam/image_prefix00051.png')
+if [ ! -d ~/snaps/ ]; then
+  mkdir ~/snaps
+
+vlc -I dummy v4l2:///dev/video0 --video-filter scene --no-audio --scene-path '/home/'$USER'/snaps' --scene-prefix image_prefix --scene-format png vlc://quit --run-time=1
+NAME = $(echo $HOME'/snaps/image_prefix00051.png')
 convert $NAME -flip current.jpg
-thunderbird -compose 'to=email@email.edu,subject=subject,attachment=file:///home/'$USER'/images/webcam/current.jpg' &
+# If you don't want the original image removed, comment out the line below
+rm $NAME
+thunderbird -compose 'to=email@email.edu,subject=subject,attachment=file:///home/'$USER'/snaps/current.jpg' &
 sleep 1
 xdotool key Control_L+Return
