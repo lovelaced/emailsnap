07a79c7dee25269ac0bfe2d04a59b1b2fcf7f609
lovelaced <ecg@unseen.is>
2015-01-23 16:21:28 +0000

    Update email.sh

--- email.sh
+++ email.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-vlc -I dummy v4l2:///dev/video0 --video-filter scene --no-audio --scene-path /home/USERNAME/images/webcam --scene-prefix image_prefix --scene-format png vlc://quit --run-time=1
+vlc -I dummy v4l2:///dev/video0 --video-filter scene --no-audio --scene-path '/home/'$USER'/images/webcam' --scene-prefix image_prefix --scene-format png vlc://quit --run-time=1
 NAME = $(echo $HOME'/images/webcam/image_prefix00051.png')
 convert $NAME -flip current.jpg
-thunderbird -compose "to=email@email.edu,subject=subject,attachment=file:///home/USER/images/webcam/current.jpg" &
+thunderbird -compose 'to=email@email.edu,subject=subject,attachment=file:///home/'$USER'/images/webcam/current.jpg' &
 sleep 1
 xdotool key Control_L+Return
